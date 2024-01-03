75bb0327c8e99278a8c7fe93238ef4a60d92a6b4
Eberson Oliveira <eberson.olivera@gmail.com>
2024-01-03 00:27:38 -0300

    fix: misplaced name

--- enterprise/src/main/java/br/com/fiap/tech/challenge/enterprise/error/ApplicationError.java
+++ enterprise/src/main/java/br/com/fiap/tech/challenge/enterprise/error/ApplicationError.java
@@ -3,19 +3,16 @@ package br.com.fiap.tech.challenge.enterprise.error;
 import br.com.fiap.tech.challenge.exception.error.BaseApplicationError;
 import br.com.fiap.tech.challenge.exception.error.ErrorType;
 
-import static br.com.fiap.tech.challenge.exception.error.ErrorType.CONFLICT;
 import static br.com.fiap.tech.challenge.exception.error.ErrorType.INTERNAL_SERVER_ERROR;
 import static br.com.fiap.tech.challenge.exception.error.ErrorType.INVALID_PARAMETER;
 import static br.com.fiap.tech.challenge.exception.error.ErrorType.NOT_FOUND;
-import static br.com.fiap.tech.challenge.exception.error.ErrorType.UNPROCESSABLE_ENTITY;
-import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 public enum ApplicationError implements BaseApplicationError {
 
     UNKNOWN_ERROR("AE-001", INTERNAL_SERVER_ERROR, TRUE, "Unexpected error [{}]"),
     PRODUCT_NOT_FOUND_BY_UUID("AE-002", INVALID_PARAMETER, TRUE, "Product not found [uuid={}]"),
-    CUSTOMER_NOT_FOUND_BY_DOCUMENT("AE-003", NOT_FOUND, TRUE, "Customer not found [document={}]"),
+    CUSTOMER_NOT_FOUND_BY_UUID("AE-003", NOT_FOUND, TRUE, "Customer not found [uuid={}]"),
     CART_NOT_FOUND_BY_UUID("AE-007", INVALID_PARAMETER, TRUE, "Cart not found [uuid={}]"),
     CART_ITEM_NOT_AVAILABLE("AE-008", INVALID_PARAMETER, TRUE, "Cart item not available [cartUuid={} cartItemUuid={}]"),
     CART_CUSTOMER_NOT_AVAILABLE("AE-009", INVALID_PARAMETER, TRUE, "Cart customer not available [customerUuid={}]"),
