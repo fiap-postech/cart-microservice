4a4df11145d8a22b377b8f2eb1dfbba04daa1030
Eberson Oliveira <eberson.olivera@gmail.com>
2024-01-12 21:32:07 -0300

    feat: moved configuration to another repository

--- infra/docker/localstack/scripts/02_create_sns.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "########### Creating sns topics ###########"
-aws --endpoint-url=http://127.0.0.1:4566 sns create-topic --profile=localstack --name local-cart-microservice-close-cart-topic
-
--- infra/docker/localstack/scripts/04_create_sns_subscriptions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "########### Creating sns subscriptions ###########"
-aws --endpoint-url=http://127.0.0.1:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-cart-microservice-close-cart-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-payment-microservice-close-cart-queue --attributes RawMessageDelivery=true
-aws --endpoint-url=http://127.0.0.1:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-cart-microservice-close-cart-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-order-microservice-close-cart-queue --attributes RawMessageDelivery=true
